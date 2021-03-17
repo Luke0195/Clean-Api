e5276ff58383793553518bce7d616d7658c465ea
Luke0195 <lucas201273@gmail.com>
2021-03-16 23:40:50 -0300

    refactor: ensure UseController use  badRequest helper

--- src/presentation/controllers/UserController.ts
+++ src/presentation/controllers/UserController.ts
@@ -1,21 +1,16 @@
  import {HttpRequest,HttpResponse} from '../protocols/Http';
+ import {badRequest} from '../helpers/HttpHelper'
  import {MissingParamError} from '../errors/MissinParamError'
  class UserController {
   handle(httpRequest: HttpRequest):HttpResponse{
-    if(!httpRequest.body.name){
-      return{
-        statusCode: 400,
-        body: new MissingParamError('name')
-    }
-  }
+   const requiredFields = ['name', 'phone_number']
 
-    if(!httpRequest.body.phone_number){
-      return{
-        statusCode: 400,
-        body: new MissingParamError('phone_number')
-      }
+  for(const field of requiredFields){
+    if(!httpRequest.body[field]){
+      return badRequest(new MissingParamError(field))
     }
   }
 }
+ }
 
 export default UserController;
---
+++ src/presentation/helpers/HttpHelper.ts
@@ -0,0 +1,7 @@
+import {HttpResponse} from '../protocols/Http'
+export const badRequest = (error:Error):HttpResponse =>{
+  return{
+    statusCode:400,
+    body: error
+  }
+}
