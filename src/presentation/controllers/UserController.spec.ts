9b29bb43b59622893d6fbc72cd645c2b949adf6c
Luke0195 <lucas201273@gmail.com>
2021-03-16 23:29:15 -0300

    refactor: add customized error for missing param

--- src/presentation/controllers/UserController.spec.ts
+++ src/presentation/controllers/UserController.spec.ts
@@ -1,3 +1,4 @@
+import {MissingParamError} from '../errors/MissinParamError'
 import UserController from './UserController';
 describe('User Controller', () => {
   test('Should returns 400 if no name is provider', () => {
@@ -14,6 +15,25 @@ describe('User Controller', () => {
 
     const httpResponse = sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError('name'))
+  });
+
+
+  test('Should returns 400 if no phone_number is provider', () => {
+    const sut = new UserController();
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@gmail.com',
+        age: 20,
+        weight: 1.80,
+        ethnicity: 'white',
+      },
+    };
+
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError('phone_number'))
   });
 
 
--- src/presentation/controllers/UserController.ts
+++ src/presentation/controllers/UserController.ts
@@ -1,10 +1,20 @@
- import {HttpRequest,HttpResponse} from '../protocols/Http'
+ import {HttpRequest,HttpResponse} from '../protocols/Http';
+ import {MissingParamError} from '../errors/MissinParamError'
  class UserController {
   handle(httpRequest: HttpRequest):HttpResponse{
-    return {
-      statusCode:400,
-      body:{}
-    };
+    if(!httpRequest.body.name){
+      return{
+        statusCode: 400,
+        body: new MissingParamError('name')
+    }
+  }
+
+    if(!httpRequest.body.phone_number){
+      return{
+        statusCode: 400,
+        body: new MissingParamError('phone_number')
+      }
+    }
   }
 }
 
